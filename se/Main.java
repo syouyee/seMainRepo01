c859e6fa5cfa1d34c88e5fa03e390490365eedb9
syouyee <43868519+syouyee@users.noreply.github.com>
2019-12-03 15:58:24 +0900

    local update
    local update

--- se/Main.java
+++ se/Main.java
@@ -1,7 +1,7 @@
 package se;
 
 // 1: remote by syouyee
-// 1: local by syouyee
+// 2: local by syouyee
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
